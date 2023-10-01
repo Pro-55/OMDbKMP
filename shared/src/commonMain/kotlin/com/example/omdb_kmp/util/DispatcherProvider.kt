fe894bc3df342b2b1d059d6f9d1553de40d2cfc4
Pranit Rane <pranitrane619@gmail.com>
2023-10-02 02:17:21 +0530

    update: arch changes [I]
    1. common coroutines dependencies added
    2. dispatcher provider added
    3. misc changes

---
+++ shared/src/commonMain/kotlin/com/example/omdb_kmp/util/DispatcherProvider.kt
@@ -0,0 +1,10 @@
+package com.example.omdb_kmp.util
+
+import kotlinx.coroutines.CoroutineDispatcher
+
+expect object DispatcherProvider {
+    val Default: CoroutineDispatcher
+    val Main: CoroutineDispatcher
+    val Unconfined: CoroutineDispatcher
+    val IO: CoroutineDispatcher
+}
