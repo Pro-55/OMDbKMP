ebe4cb764af63478896f0f040ee6d588cbc50830
Pranit Rane <pranitrane619@gmail.com>
2023-10-03 19:01:58 +0530

    update: ios: navigation changes
    1. route enum added
    2. nav host with navigation stack impl
    3. placeholder screens for signup and home added
    4. updated gitignore for the ios app
    5. misc changes

---
+++ iosApp/iosApp/ui/home/HomeScreen.swift
@@ -0,0 +1,16 @@
+//
+//  HomeScreen.swift
+//  iosApp
+//
+//  Created by Pranit Rane on 02/10/23.
+//  Copyright © 2023 orgName. All rights reserved.
+//
+import SwiftUI
+
+struct HomeScreen: View {
+    @Binding var stack: [Route]
+    
+    var body: some View {
+        Text("Home!")
+    }
+}
