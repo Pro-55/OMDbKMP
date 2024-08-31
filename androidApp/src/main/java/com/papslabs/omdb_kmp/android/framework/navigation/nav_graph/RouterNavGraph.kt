67a5de4c3239062a52dba9a22602cb24373d6c4f
Pranit Rane <pranitrane619@gmail.com>
2024-08-31 19:55:31 +0530

    update: router logic impl [android]
    1. router screen + view model added
    2. di changes

--- androidApp/src/main/java/com/papslabs/omdb_kmp/android/framework/navigation/nav_graph/RouterNavGraph.kt
+++ androidApp/src/main/java/com/papslabs/omdb_kmp/android/framework/navigation/nav_graph/RouterNavGraph.kt
@@ -1,15 +1,12 @@
 package com.papslabs.omdb_kmp.android.framework.navigation.nav_graph
 
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.navigation.NavController
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
 import com.papslabs.omdb_kmp.android.framework.navigation.Route
 import com.papslabs.omdb_kmp.android.framework.navigation.Screen
+import com.papslabs.omdb_kmp.android.ui.router.RouterScreen
 import com.papslabs.omdb_kmp.android.util.extensions.navigateWithPopUpTo
 
 fun NavGraphBuilder.routerNavGraph(navController: NavController) {
@@ -17,15 +14,23 @@ fun NavGraphBuilder.routerNavGraph(navController: NavController) {
         route = Route.Router.name,
         startDestination = Screen.Router.route
     ) {
-        composable(route = Screen.Router.route) {
-            // TODO: ADD SHARED PREF LOGIC
-            val key by remember { mutableStateOf(false) }
-            LaunchedEffect(key) {
-                navController.navigateWithPopUpTo(
-                    route = Route.Auth.name,
-                    popUpTo = Route.Router.name
-                )
-            }
+        composable(
+            route = Screen.Router.route
+        ) {
+            RouterScreen(
+                navigateRouterToSignUp = {
+                    navController.navigateWithPopUpTo(
+                        route = Route.Auth.name,
+                        popUpTo = Route.Router.name
+                    )
+                },
+                navigateRouterToHome = {
+                    navController.navigateWithPopUpTo(
+                        route = Route.App.name,
+                        popUpTo = Route.Router.name
+                    )
+                }
+            )
         }
     }
 }
