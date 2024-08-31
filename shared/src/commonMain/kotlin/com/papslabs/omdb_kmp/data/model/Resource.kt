1be25d7607692e418e4059b31d9e893d29072252
Pranit Rane <pranitrane619@gmail.com>
2024-08-31 18:59:47 +0530

    update: misc changes
    1. resource sealed class updated to sealed interface
    2. misc import changes

--- shared/src/commonMain/kotlin/com/papslabs/omdb_kmp/data/model/Resource.kt
+++
@@ -1,7 +0,0 @@
-package com.papslabs.omdb_kmp.data.model
-
-sealed class Resource<T>(val msg: String? = null, val data: T? = null) {
-    class Loading<T> : Resource<T>()
-    class Success<T>(data: T?) : Resource<T>(data = data)
-    class Error<T>(msg: String?) : Resource<T>(msg = msg)
-}
